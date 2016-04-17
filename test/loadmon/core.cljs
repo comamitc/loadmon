23baddfdc82c1e0295370504360d056de5122a30
Mitch Comardo <comamitc@gmail.com>
2016-04-17 16:00:56 -0500

    SAVEPOINT: work on converting js objects into cli map

--- test/loadmon/core.cljs
+++ test/loadmon/core.cljs
@@ -2,14 +2,32 @@
   (:require-macros
     [cljs.core.async.macros :refer [go]])
   (:require [cljs.nodejs :as nodejs]
-            [loadmon.reader.gpx :as gpx]
+            [loadmon.parse :refer [parse]]
             [cljs.core.async :refer [<!]]))
 
 (nodejs/enable-util-print!)
 
+(defn foo
+  [o]
+  (loop [x o
+         ks (.keys js/Object x)
+         acc {}]
+    (let [k (first ks)
+          v (aget x k)]
+      (vector (keyword k)
+              v))))
+
+(defn jso->clj [x]
+  (reduce (fn [acc curr]
+            (let [v (aget x curr)]
+              (assoc acc (keyword curr) v)))
+          {}
+          (.keys js/Object x)))
+
+
 (defn -test []
   (go
-    (let [resp (<! (gpx/load-file "resources/examples/example-001.gpx"))]
-      (print resp))))
+    (let [resp (<! (parse :gpx :rtss "resources/examples/example-001.gpx"))]
+      (print (jso->clj (:data resp))))))
 
 (set! *main-cli-fn* -test)
