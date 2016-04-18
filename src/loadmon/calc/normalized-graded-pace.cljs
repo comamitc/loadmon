e9591cc76afd1af733c476486344a5cc3e55f0e5
Mitch Comardo <comamitc@gmail.com>
2016-04-18 13:26:01 -0500

    normalized graded pace calculations

---
+++ src/loadmon/calc/normalized-graded-pace.cljs
@@ -0,0 +1,30 @@
+(ns loadmon.calc.normalized-graded-pace
+  (:require [loadmon.calc.common :refer [atan to-deg]]
+            [tubax.helpers :as th]))
+
+(defn- get-ele [n]
+  (-> n
+      (th/find-first {:tag :ele})
+      (th/text)))
+
+(defn- grade [start end dist]
+  (let [ele-chg (- end start)
+        rad     (atan (/ ele-chg (* dist 1000)))]
+    (to-deg rad)))
+
+(defn- decline-pace [pace grade]
+  (let [coef 0.01815]
+    (/ pace (+ 1 (* coef grade)))))
+
+(defn- incline-pace [pace grade]
+  (let [coef 0.033]
+    (/ pace (+ 1 (* coef grade)))))
+
+(defn calc-gap [last curr interval]
+  (let [s-ele (get-ele last)
+        e-ele (get-ele curr)
+        p     (:pace interval)
+        g     (grade s-ele e-ele (:dist interval))]
+    (assoc interval :gap (if (> g 0)
+                            (incline-pace p g)
+                            (decline-pace p g)))))
