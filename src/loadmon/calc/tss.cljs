4f5610af774ba829822835602f423ec222d0f507
Mitch Comardo <comamitc@gmail.com>
2016-04-18 15:07:21 -0500

    complete tss formula

--- src/loadmon/calc/tss.cljs
+++ src/loadmon/calc/tss.cljs
@@ -1,11 +1,15 @@
 (ns loadmon.calc.tss)
 
 (defn- avg-gap [intervals]
-  (let [sum (reduce #(+ %1 %2) 0 intervals)]
+  (let [sum (reduce #(+ %1 (:ngp %2)) 0 intervals)]
     (/ sum (count intervals))))
 
-(defn- if* [ngp ftp]
+(defn- *if* [ngp ftp]
   (/ ngp ftp))
 
-(defn r-tss [gpx-data ftp]
-  nil)
+(defn r-tss [data ftp]
+  (let [ngp  (avg-gap (:intervals data))
+        secs (/ (:ttl-time data) 1000)
+        num  (* secs ngp (*if* ngp ftp))
+        den  (* ftp 3600)]
+    (* (/ num den) 100)))
